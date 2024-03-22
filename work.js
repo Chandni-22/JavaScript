5e70d84359a7a65910043f779bebe3d367933a00
Chandni Vishwakarma <chandni22@navgurukul.org>
2024-03-22 15:37:45 +0530

    first commit

---
+++ work.js
@@ -0,0 +1,13 @@
+let a=8;
+let b=3;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+console.log(a%b)
+
+let may=31;
+let hours=24;
+let sec=60*60;
+console.log("Total seconds in may month =",may*hours*sec)
