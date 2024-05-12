d0dc0d70f0b7a1bee90fd7c45ba8a2a01709e0e2
Chandni Vishwakarma <chandni22@navgurukul.org>
2024-05-12 22:36:52 +0530

    Added JS Info

---
+++ info.js
@@ -0,0 +1,65 @@
+//---------->What is JavaScript?<----------//
+
+// 1.)JavaScript is a Scripting Language.
+// 2.)It is a single-threaded programming language.
+// 3.)It runs Code line by line.
+// 4.)JavaScript is a Dynamically type language.
+
+
+//---------->What is Dynamically typed language?<----------//
+
+// A dynamically typed language is a programming language where variables are not bound to a specific data type at compile time but rather their type is determined at runtime. In dynamically typed languages, variables can hold values of any type, and their types can change as the program executes. Examples of dynamically typed languages include Python, JavaScript, Ruby, and PHP.
+
+
+//Web-Dev:
+// HTML - Create Page Strucure
+// CSS - Add Style(Static WebPage)
+// JS - Make our page Live(Interactive)(Dynamic Webpage)
+
+// Programs - Script, Set of Instruction (Cook for me).
+
+
+//---------->History:<----------//
+
+// 1.)Brendan Eich - 1995 (10 Days)(Mocha).
+// 2.)LiveScript2.)(initial name) -> Javascript.
+
+
+// JavaScript - frontend (React JS)/ backend (Node.js)
+// Client / Server
+
+
+//---------->Js Engine:<----------//
+
+// V8 - Chorme, Opera, MS Edge
+// SpiderMonkey - Firefox
+
+// What Engine Do:-> Read Script, Complilar convert it into Machine Code.
+
+
+//---------->ECMAScript:<----------//
+
+// 1.)ECMAScript is the standard specification for JavaScript.
+// 2.)It defines the rules and features that JavaScript should follow.
+// 3.)It Ensure Compatablity between diffrent browsers.
+// 4.)In simpler terms, ECMAScript tells us how JavaScript should work.
+// 5.)It is a standard for scripting languages, including JavaScript, JScript, and ActionScript.
+
+
+//---------->Ways of Adding JS:<----------// 
+
+// 1.)Internal: In this method, JavaScript code is directly written within the HTML document using the <script> tag. Inline scripts are useful for small snippets of code or for executing specific actions directly within the HTML document.
+// Example: <script>
+//             alert('Hello, world!');
+//         </script>
+
+// 2.)External: JavaScript code is placed in a separate file with a .js extension, and then linked to the HTML document using the <script> tag's src attribute. External scripts are beneficial for organizing and reusing code.
+// Example: <script src="script.js"></script>
+
+
+//---------->Type Of Comments in JS:<----------//
+
+// 1.)Single Line: Starting of the line with "//".
+// 2.)Multi Line Comment: Multi-line comments start with "/*" and end with "*/". Used for commenting multiple lines.
+
+// Use of Comment: These comments are not executed by the JavaScript engine and are purely for human readability. They are useful for documenting code, explaining functionality, or temporarily disabling code segments during testing or debugging.
