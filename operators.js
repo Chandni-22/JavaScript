e2495ef1c430a4453a550b9ea94d94cd9276ae19
Chandni Vishwakarma <chandni22@navgurukul.org>
2024-05-14 20:41:08 +0530

    Arithmatic Operators

---
+++ operators.js
@@ -0,0 +1,82 @@
+//---------->Arithmatic Operators<----------//
+
+// BODMAS: left to right
+
+// 1.)Addition: ('+') -> Adds two operands.
+
+// let num1 = 10;
+// let num2 = 3;
+// console.log(num1+num2);
+
+// 2.)Subtraction: ('-') -> Subtracts the second operand from the first.
+
+// let num1 = 10;
+// let num2 = 3;
+// console.log(num1-num2);
+
+// 3.)Multiplication: ('*') -> Multiplies two operands.
+
+// let num1 = 10;
+// let num2 = 3;
+// console.log(num1*num2);
+
+// 4.)Division: ('/') -> Divides the first operand by the second.
+
+// let num1 = 10;
+// let num2 = 3;
+// console.log(num1/num2);
+
+// 5.)Modulus: ('%') -> Returns the remainder.
+
+// let num1 = 10;
+// let num2 = 3;
+// console.log(num1%num2);
+
+// 6.)Exponentiation: ('**') -> Raises the first operand to the power of the second.
+
+// let num1 = 10;
+// let num2 = 3;
+// console.log(num1**num2);
+
+// Math.pow() do the same as ('**').
+// console.log(Math.pow(num6,2))
+
+
+//---------->Increment and Decrement Operators<----------//
+
+// 1.)Increment(++):
+// post increment -> first return then increment
+// pre increment -> first increment then return
+
+// let a=8;
+// console.log(a++); //8
+// console.log(++a); //10
+
+// 2.)Decrement(--):
+// post decrement -> first return then decrement
+// pre decrement -> first decrement then return
+
+// let a=8;
+// console.log(a--); //8
+// console.log(--a); //10
+
+
+// String + Number = String: (+) concatenation operator
+
+// let num=8;
+// console.log(num+"first"); //"8first"
+
+// console.log(2+2+"2"+2); //"422"
+
+// console.log(10 + 8 + "8"); //"188";
+
+// console.log(10 + 5 + "Hello" + 9 + 9);  // 10 + 5 = 15 + "Hello" = 15Hello + 9 = 15Hello9 + 9 = 15Hello99
+// left to right
+
+// let num10 = 10;
+// let num11 = 20;
+// let word = "ten";
+// console.log(num10 + num11 + word);
+
+
+//---------->Assignment Operators<----------//
